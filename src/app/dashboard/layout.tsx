40722b3a19cc59bafebfe6dfdec7fe8e66a84262
Nguyen Huy Vu <vunhhe161239@fpt.edu.vn>
2025-03-20 17:10:58 +0700

    add chart and admin layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,14 +9,24 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <div className="h-screen flex">
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-          <Link href="/" className="flex items-center justify-center gap-2">
-          <Image src="/logo.png" alt="logo" width={32} height={32}/>
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center gap-2 lg:justify-start"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">SchoolKat</span>
-          </Link>
-        </div>
-        <div className="w-[86%] md:w-[92%] lg:w-[86%] xl:w-[86%] bg-blue-200">r</div>
+        </Link>
+        <Menu/>
       </div>
+
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[86%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+          <Navbar/> 
+          {children}
+      </div>
+    </div>
   );
 }
